fa86c171c58b831609243790464250bc09dc294c
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2021-05-12 19:51:13 -0300

    feat: add storybook config

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,15 +1,16 @@
 import * as S from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'Next Boilerplate',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components'
+}) => (
   <S.Wrapper>
     <S.Logo
       src="/img/logo.svg"
       alt="Imagem de um átomo e React Avançado escrito ao lado."
     />
-    <S.Title>Next Boilerplate</S.Title>
-    <S.Description>
-      TypeScript, ReactJS, NextJS e Styled Components
-    </S.Description>
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
     <S.Illustration
       src="/img/hero-illustration.svg"
       alt="Um desenvolvedor de frente para uma tela com código."
