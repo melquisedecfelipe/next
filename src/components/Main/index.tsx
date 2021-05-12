7053744819759ad55b74dd54db3c27e96c0f7545
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2021-05-12 18:44:04 -0300

    feat: add main component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,20 @@
-const Main = () => {
-  return (
-    <main>
-      <h1>Next Boilerplate</h1>
-    </main>
-  )
-}
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+    <S.Title>Next Boilerplate</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código."
+    />
+  </S.Wrapper>
+)
 
 export default Main
