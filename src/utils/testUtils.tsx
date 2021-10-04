10f433643c80fbfe11e052111aa310a31afba516
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2021-10-04 16:40:35 -0300

    feat: add test util

---
+++ src/utils/testUtils.tsx
@@ -0,0 +1,14 @@
+import { ReactElement } from 'react'
+
+import { render } from '@testing-library/react'
+
+import { ThemeProvider } from 'styled-components'
+
+import theme from 'styles/theme'
+
+const customRender = (ui: ReactElement) =>
+  render(<ThemeProvider theme={theme}>{ui}</ThemeProvider>)
+
+export * from '@testing-library/react'
+
+export { customRender as render }
