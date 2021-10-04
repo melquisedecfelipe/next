a8ca60f2e776895bcb572932f675e911f9edffd3
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2021-10-04 16:41:34 -0300

    feat: update coverage config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,15 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!src/**/styles.ts',
+    '!src/pages/**/*.ts(x)?',
+    '!src/styles/**',
+    '!src/types/**',
+    '!**/*.d.ts'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/']
 }
